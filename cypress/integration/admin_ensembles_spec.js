69f9d1e0fbcb8b939fabada71a15b6489cf1aa17
Matt Wayne <matt@thinkdolabs.com>
2020-04-14 02:18:01 -0700

    TBC.

--- cypress/integration/admin_ensembles_spec.js
+++ cypress/integration/admin_ensembles_spec.js
@@ -4,7 +4,7 @@ const itemTitles = {home: 'Dashboard', ensembles: 'Ensembles', series: 'Series',
 const textFieldEdit = 'Test'
 
 
-describe ("Test Ensembles tab in Admin", function () {
+describe ("Navigate to profile page and confirm there is data", function () {
     it("Ensembles tab is visible, can be clicked and routes to Ensembles page", function () {
       cy.visit(baseUrl)
         .contains(itemTitles.home)
@@ -24,16 +24,16 @@ describe ("Test Ensembles tab in Admin", function () {
     })
     it("Table contains data", function () {
       cy.get('tbody>tr')
-        .get('td.text-left') // make selector. this is the data content of each cell in table body (i.e. does not including the checkboxes in first column)
-        .children('a') // make selector. these are the data links in the Name column (note: for articles this would be ID column)
+        .get('td.text-left')
+        .children('a')
         // .get('td.text-left')
-        // .children('div').children('a') - make selector. these are the data links in subsequent columns
+        // .children('div').children('a')
         .should('be.visible')
     })
     it("Clicking data link in table routes to profile", function() {
-      cy.get('tbody>tr') // make selector? this is getting all table rows in table body
-        .children('.text-left') // make selector. this is the text content of each cell in table body (i.e. does not including the checkboxes in first column)
-        .children('a') // make selector. these are the links in the Name column (note: for articles this would be ID column)
+      cy.get('tbody>tr') 
+        .children('.text-left') 
+        .children('a') 
         // .get('td.text-left')
         // .children('div').children('a') - make selector. these are the data links in subsequent columns
         .eq(0).click()
@@ -48,49 +48,82 @@ describe ("Test Ensembles tab in Admin", function () {
       }) 
     })
     it("Profile page contains data", function () {
-      cy.get('div.v-content__wrap') // make selector? this is the content wrapper
-        .children('.container.pl-5.align-start') // make selector? this is the container
-        .children('div') //see comment in last line of test suite
-        .children()
+      cy.get('div.pl-0.pr-10.col-lg-10.col-xl-10.col') 
+        .children('div') 
         .children('div.row')
         .children('div.col')
         .children('div.row')
         .children('div.col').not('div.font-weight-black.col-md-4.col')
-        .children().should('be.visible') // all code above gets the Description, Genres...Release Date fields with the exception of Location. This is because they have the same selector as the column names which I excluded with the { .not('div.font-weight-black.col-md-4.col') } command.
+        .children().should('be.visible') 
+      })
     })
+describe ("Edit profile, save update, confirm saving returns to profile page and update has rendered", function () {
     it("Edit button is visible, can be clicked and routes to edit page", function () {
-      cy.get('span.v-btn__content') // make selector - this is the Edit button
-      .contains('Edit')
-      .should('be.visible')
-      .click()
-      .wait(3000)
-      .url().then(url => {
-        const ensembleIdString = url.split('/').slice(-2)[0]
-        const ensembleIdInteger = parseInt(ensembleIdString)
-        cy.location().should((loc) => {
-          expect(loc.origin).to.eq(baseUrl)
-          expect(loc.pathname).to.eq(pagePaths.ensembles + "/" + ensembleIdInteger + pagePaths.edit) 
-      })
-    }) 
-  })
+      cy.get('span.v-btn__content').contains('Edit')
+        .should('be.visible')
+        .click()
+        .wait(3000)
+        .url().then(url => {
+          const ensembleIdString = url.split('/').slice(-2)[0]
+          const ensembleIdInteger = parseInt(ensembleIdString)
+          cy.location().should((loc) => {
+            expect(loc.origin).to.eq(baseUrl)
+            expect(loc.pathname).to.eq(pagePaths.ensembles + "/" + ensembleIdInteger + pagePaths.edit) 
+          })
+        })
     it("Edit name, click Save button and confirm that it returns to profile page and edit has rendered", function () {
-      cy.get('div.v-text-field__slot').children('input').first() // make selector? this gets the first text input - the Name field in this instance
-      .clear()
-      .type(textFieldEdit) //put a selector for the form it self (profile form as the class for example). selector could be ".profile-form.name"
-      .get('button.float-right.blue.darken-2.white--text.mt-2.v-btn.v-btn--contained.theme--light.v-size--default') // make selector. this is the Save button
-      .should('be.visible')
-      .contains('Save')
-      .click()
-      .wait(3000)
-      .get('div.pl-0.pr-10.col-lg-10.col-xl-10.col') // make selector. this is the content wrapper
-      .children('h1').contains(textFieldEdit) // this is the item header - the ensemble name in this instance
-      .url().then(url => {
-        const ensembleIdString = url.split('/').slice(-2)[0]
-        const ensembleIdInteger = parseInt(ensembleIdString)
-        cy.location().should((loc) => {
-          expect(loc.origin).to.eq(baseUrl)
-          expect(loc.pathname).to.eq(pagePaths.ensembles + "/" + ensembleIdInteger + "/")
+      cy.get('div.v-text-field__slot').children('input').first()
+        .clear()
+        .type(textFieldEdit) 
+        .get('button.float-right.blue.darken-2.white--text.mt-2.v-btn.v-btn--contained.theme--light.v-size--default') 
+        .should('be.visible')
+        .contains('Save')
+        .click()
+        .wait(3000) 
+        .url().then(url => {
+          const ensembleIdString = url.split('/').slice(-2)[0]
+          const ensembleIdInteger = parseInt(ensembleIdString)
+          cy.location().should((loc) => {
+            expect(loc.origin).to.eq(baseUrl)
+            expect(loc.pathname).to.eq(pagePaths.ensembles + "/" + ensembleIdInteger + "/")
+        })
+        .get('div.pl-0.pr-10.col-lg-10.col-xl-10.col')
+        .children('h1').contains(textFieldEdit)
       })
-    }) 
+    })
   })
 })
+describe ("Return profile to original state", function () {
+    before(function () {
+      cy.get('div.v-text-field__slot').children('input').first()
+      .invoke('text').as('text')
+    })
+    it("Reverse the changes made in previous edit, save and confirm changes", function () {
+      cy.get('span.v-btn__content').contains('Edit')
+        .click()
+        .wait(3000)
+        .url().then(url => {
+          const ensembleIdString = url.split('/').slice(-2)[0]
+          const ensembleIdInteger = parseInt(ensembleIdString)
+          cy.location().should((loc) => {
+            expect(loc.origin).to.eq(baseUrl)
+            expect(loc.pathname).to.eq(pagePaths.ensembles + "/" + ensembleIdInteger + pagePaths.edit) 
+        })
+        .get('div.v-text-field__slot').children('input').first()
+        .type(this.text)
+        .get('button.float-right.blue.darken-2.white--text.mt-2.v-btn.v-btn--contained.theme--light.v-size--default') 
+        .click()
+        .wait(3000)
+        .url().then(url => {
+          const ensembleIdString = url.split('/').slice(-2)[0]
+          const ensembleIdInteger = parseInt(ensembleIdString)         
+          cy.location().should((loc) => {
+            expect(loc.origin).to.eq(baseUrl)
+            expect(loc.pathname).to.eq(pagePaths.ensembles + "/" + ensembleIdInteger + "/")
+        })
+        .get('div.pl-0.pr-10.col-lg-10.col-xl-10.col')
+        .children('h1').contains(this.text)  
+        }) 
+      })
+    })
+  })
